d33bb36d66d81105b076fbc4b975867b89c2afb9
squadcincodois <squadrecode.52.pro@gmail.com>
2022-09-26 23:24:46 -0300

    Primeiro commit

---
+++ GoodTravel_BD.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE  IF NOT EXISTS `agencia` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `agencia`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: agencia
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `destinos`
+--
+
+DROP TABLE IF EXISTS `destinos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `destinos` (
+  `idDestino` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `estado` varchar(40) NOT NULL,
+  `pais` varchar(40) NOT NULL,
+  `dataIda` varchar(40) NOT NULL,
+  `dataVolta` varchar(40) NOT NULL,
+  `valor` float NOT NULL,
+  PRIMARY KEY (`idDestino`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `destinos`
+--
+
+LOCK TABLES `destinos` WRITE;
+/*!40000 ALTER TABLE `destinos` DISABLE KEYS */;
+INSERT INTO `destinos` VALUES (1,'São Paulo','SP','Brasil','02/09','31/11',479);
+/*!40000 ALTER TABLE `destinos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promocoes`
+--
+
+DROP TABLE IF EXISTS `promocoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promocoes` (
+  `idPromo` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `valorPromo` float NOT NULL,
+  `idDestino` int NOT NULL,
+  PRIMARY KEY (`idPromo`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promocoes`
+--
+
+LOCK TABLES `promocoes` WRITE;
+/*!40000 ALTER TABLE `promocoes` DISABLE KEYS */;
+INSERT INTO `promocoes` VALUES (1,'Black Friday',500,0);
+/*!40000 ALTER TABLE `promocoes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-04 17:54:08
